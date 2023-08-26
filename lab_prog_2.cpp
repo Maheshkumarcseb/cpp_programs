1bc88d793bf0e80ef1d0621876ca4c42fded1b3c
Mahesh kumar <mahesh.mahi24102004@gmail.com>
2023-08-26 23:16:35 +0530

    first commit

---
+++ lab_prog_2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,isum=0,i;
+    cout<<"enter the value of n ";
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        isum+=i;
+    }
+    cout<<"sum of 1 to "<<n<<"is :"<<isum<<endl;
+    cout<<"sum of 1 t0 "<<n<<"using formula is "<<(n*(n+1)/2)<<endl;
+    return 0;
+}
