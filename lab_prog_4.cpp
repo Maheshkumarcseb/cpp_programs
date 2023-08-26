de1a5b165e8ee3ae4cdf1c950b2027743a7e6a29
Mahesh kumar <mahesh.mahi24102004@gmail.com>
2023-08-26 23:48:43 +0530

    function overloading

---
+++ lab_prog_4.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+void add(int ,int);
+void add(double ,double);
+int main()
+{
+    int num1,num2;
+    double d1,d2;
+    cout<<"enter 2 integer value\n";
+    cin>>num1>>num2;
+    cout<<"enter two double value";
+    cin>>d1>>d2;
+    add(num1,num2);
+    add(d1,d2);
+    return 0;
+}    
+void add(int p,int q)
+{
+    int sum;
+    sum=p+q;
+    cout<<"integer addition "<<sum<<endl;
+}
+void add(double p,double q)
+{
+    double sum;
+    sum=p+q;
+    cout<<"double value addition "<<sum<<endl;
+}
